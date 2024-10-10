5cb8bf24e93056a46ff73db84462cab8ec246637
Your Name <jaymesbubba14@gmail.com>
2024-10-10 18:05:41 +0300

    Google Gemini AI - Clone

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
